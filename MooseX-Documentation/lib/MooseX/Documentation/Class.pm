1cbe346535bdfce891cfeb17acc0390f93c565cd
Kent Fredric <kentfredric@gmail.com>
2009-05-22 16:23:43 +1200

    podification

--- MooseX-Documentation/lib/MooseX/Documentation/Class.pm
+++ MooseX-Documentation/lib/MooseX/Documentation/Class.pm
@@ -42,47 +42,26 @@ sub add_method
     my $name    = $params{name};
     my %options = %{ $params{options} };
 
-    my $method = MooseX::Documentation::Method->new( name => $name, brief => $self->trim_whitespace( $options{brief} ) );
+    my $method = MooseX::Documentation::Method->new(
+        name  => $name,
+        brief => $self->trim_whitespace( $options{brief} )
+    );
     delete $options{brief};
     $method->unspecified( \%options );
-    $self->methods->{ $name } = $method;
+    $self->methods->{$name} = $method;
     return $self;
 }
-sub method 
+
+sub method
 {
     my $self = shift;
     return map { $self->methods->{$_} } @_;
 }
 
-sub as_pod
+sub to_pod
 {
     my $self = shift;
     return $self->_pod_h1('methods');
-}
-
-sub _text
-{
-    my $self     = shift;
-    my $title    = shift;
-    my $subtitle = shift;
-    my $field    = shift;
-    my $data     = $self->{$title}->{$subtitle}->{$field};
-    my $out      = '';
-    my $prefix   = '';
-
-    if ( $field =~ m/^example/ )
-    {
-        $prefix = q{ } x 4;
-    }
-    if ( not ref $data )
-    {
-        $out .= $prefix . $data;
-    }
-    if ( ref $data eq 'ARRAY' )
-    {
-        $out .= $prefix . $_ . "\n" for @{$data};
-    }
-    return $out;
 }
 
 sub _pod_h1
@@ -90,7 +69,7 @@ sub _pod_h1
     my $self  = shift;
     my $title = shift;
     return sprintf qq{\n=head1 %s\n\n%s}, uc($title), join '',
-      map { $self->_pod_h2( $title, $_ ) } keys %{ $self->{$title} };
+      map { $_->to_pod } values %{ $self->methods };
 }
 
 sub _pod_h2
--- MooseX-Documentation/lib/MooseX/Documentation/Method.pm
+++ MooseX-Documentation/lib/MooseX/Documentation/Method.pm
@@ -4,30 +4,69 @@ package MooseX::Documentation::Method;
 use strict;
 use warnings;
 use Moose;
-our $VERSION='0.0100';
+our $VERSION = '0.0100';
 
 use namespace::clean -except => [qw( meta )];
 
-
 has name => (
-        isa => 'Str', 
-        required => 1,
-        is => 'ro', 
+    isa      => 'Str',
+    required => 1,
+    is       => 'ro',
 );
 
 has brief => (
-        isa => 'Str|ArrayRef[Str]', 
-        required => 1,
-        is  => 'ro',
+    isa      => 'Str|ArrayRef[Str]',
+    required => 1,
+    is       => 'ro',
 );
 
 has unspecified => (
-        isa => 'HashRef[ Str | ArrayRef[Str] ]',
-        default => sub { +{} },
-        is => 'rw',
+    isa     => 'HashRef[ Str | ArrayRef[Str] ]',
+    default => sub { +{} },
+    is      => 'rw',
 );
 
+sub to_pod
+{
+    my $self = shift;
 
+    my $t = qq{\n=head2 %s\n\n%s\n\n=over 4\n\n%s\n=back\n};
+
+    my $it = qq{\n=item %s\n%s\n};
+    my $extra = '';
+    for ( sort keys %{ $self->unspecified } )
+    {
+        $extra .= sprintf $it, $_, $self->_text( $_, $self->unspecified->{$_} );
+    }
+
+    return
+      sprintf( $t, $self->name, $self->_text( 'brief', $self->brief ), $extra );
+
+}
+
+sub _text
+{
+    my $self = shift;
+    my $type = shift;
+    my $data = shift;
+
+    my $out    = '';
+    my $prefix = '';
+
+    if ( $type =~ m/^example/ )
+    {
+        $prefix = q{ } x 4;
+    }
+    if ( not ref $data )
+    {
+        $out .= $prefix . $data;
+    }
+    if ( ref $data eq 'ARRAY' )
+    {
+        $out .= $prefix . $_ . "\n" for @{$data};
+    }
+    return $out;
+}
 
 1;
 
